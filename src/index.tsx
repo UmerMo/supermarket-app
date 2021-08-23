416772e160cd2e13f043fbecba099290e403c9a2
UmerMo <umermo23@gmail.com>
2021-08-23 23:55:05 +0100

    Add products page to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ProductsPage from './pages/products/products';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductsPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
